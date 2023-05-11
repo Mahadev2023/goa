f007dc3fb59bd71b206845cf39c394a94b2736b3
Godbole Mahadev <godbolemahadevkanaka@gmail.com>
2023-05-11 15:36:30 +0530

    new contain

---
+++ goa/mypackage/dog/main.go
@@ -0,0 +1,8 @@
+// ths package doc for dog info
+package dog
+
+// Years is function which take the human year
+// and convert it into the dog year
+func Years(year int) int {
+	return 7 * year
+}
---
+++ goa/mypackage/go.mod
@@ -0,0 +1,3 @@
+module github.com/Mahadev2023/goa/mypackage
+
+go 1.20
